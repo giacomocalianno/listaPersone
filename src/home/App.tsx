600e8483ff67c1ded62780e9719d09274517c4d3
giacomo.calianno <caliannog@gmail.com>
2021-09-06 15:58:03 +0200

    Aggiornato useEffect con async/await Creazione custom hook

--- src/home/App.tsx
+++ src/home/App.tsx
@@ -3,6 +3,7 @@ import '../App.css';
 import HomeContainer from "./HomeContainer";
 import Intestazione from "./Intestazione"
 import {CircularProgress, Container, makeStyles} from "@material-ui/core";
+import useProva from "./useProva";
 
 export interface IPerson {
     createdAt: string
@@ -14,80 +15,6 @@ export interface IPerson {
     birthCoutry: string
     id: string
 }
-
-// const people: IPerson[] = [{
-//     "createdAt": "2021-08-31T11:40:55.674Z",
-//     "name": "Katrine",
-//     "avatar": "https://cdn.fakercloud.com/avatars/goddardlewis_128.jpg",
-//     "surname": "Tillman",
-//     "birthDate": " 1",
-//     "birthCity": "Port Danikaland",
-//     "birthCoutry": "Borders",
-//     "id": "1"
-// }, {
-//     "createdAt": "2021-08-31T23:36:16.894Z",
-//     "name": "Alana",
-//     "avatar": "https://cdn.fakercloud.com/avatars/marosholly_128.jpg",
-//     "surname": "Dickens",
-//     "birthDate": "birthDate 2",
-//     "birthCity": "High Point",
-//     "birthCoutry": "Cambridgeshire",
-//     "id": "2"
-// }, {
-//     "createdAt": "2021-08-31T20:14:53.467Z",
-//     "name": "Jaqueline",
-//     "avatar": "https://cdn.fakercloud.com/avatars/1markiz_128.jpg",
-//     "surname": "Hirthe",
-//     "birthDate": "birthDate 3",
-//     "birthCity": "O'Haraland",
-//     "birthCoutry": "Buckinghamshire",
-//     "id": "3"
-// }, {
-//     "createdAt": "2021-09-01T06:39:41.547Z",
-//     "name": "Scarlett",
-//     "avatar": "https://cdn.fakercloud.com/avatars/miguelmendes_128.jpg",
-//     "surname": "Mertz",
-//     "birthDate": "birthDate 4",
-//     "birthCity": "North Isadore",
-//     "birthCoutry": "Buckinghamshire",
-//     "id": "4"
-// }, {
-//     "createdAt": "2021-08-31T17:03:16.941Z",
-//     "name": "Peggie",
-//     "avatar": "https://cdn.fakercloud.com/avatars/mwarkentin_128.jpg",
-//     "surname": "Emard",
-//     "birthDate": "birthDate 5",
-//     "birthCity": "South Carmelland",
-//     "birthCoutry": "Cambridgeshire",
-//     "id": "5"
-// }, {
-//     "createdAt": "2021-09-01T10:48:40.845Z",
-//     "name": "Bryce",
-//     "avatar": "https://cdn.fakercloud.com/avatars/rpeezy_128.jpg",
-//     "surname": "Hackett",
-//     "birthDate": "birthDate 6",
-//     "birthCity": "East Antonina",
-//     "birthCoutry": "Cambridgeshire",
-//     "id": "6"
-// }, {
-//     "createdAt": "2021-09-01T05:32:45.332Z",
-//     "name": "Providenci",
-//     "avatar": "https://cdn.fakercloud.com/avatars/leehambley_128.jpg",
-//     "surname": "Volkman",
-//     "birthDate": "birthDate 7",
-//     "birthCity": "Port Chetborough",
-//     "birthCoutry": "Bedfordshire",
-//     "id": "7"
-// }, {
-//     "createdAt": "2021-09-01T01:54:24.704Z",
-//     "name": "Stefanie",
-//     "avatar": "https://cdn.fakercloud.com/avatars/hafeeskhan_128.jpg",
-//     "surname": "Streich",
-//     "birthDate": "birthDate 8",
-//     "birthCity": "Aurora",
-//     "birthCoutry": "Borders",
-//     "id": "8"
-// }];
 
 const useStyles = makeStyles({
     center: {
@@ -101,34 +28,43 @@ const useStyles = makeStyles({
 
 const App: React.FC = props => {
 
-    const classes = useStyles();
     const [people, setPeople] = useState<IPerson[]>();
     const [fetching, setFetching] = useState<Boolean>(true);
     const [error, setError] = useState<Boolean>(false);
+    const classes = useStyles();
 
     useEffect(() => {
         // per far vedere che funziona lo spinner chiamo la fetch dopo 2 secondi
         // fetch
-        fetch("https://612f5b495fc50700175f159f.mockapi.io/api/users")
-            .then(res => {
-                if (!res.ok) {
-                    throw Error("Errore nel caricamento dei dati")
-                }
-                return res.json()
-            })
-            .then(data => {
-                // salvo le persone
-                setPeople(data);
-            })
-            .catch(err => {
-                    setError(true);
-                    console.log(`Errore: ${err.message}`)
-                }
-            )
+        (async () => {
+            const res = await fetch("https://612f5b495fc50700175f159f.mockapi.io/api/users")
+            if (!res.ok) {
+                setError(true)
+            }
+            const data = await res.json();
+            setPeople(data)
+            // .then(res => {
+            //     if (!res.ok) {
+            //         throw Error("Errore nel caricamento dei dati")
+            //     }
+            //     return res.json()
+            // })
+            // .then(data => {
+            //     // salvo le persone
+            //     setPeople(data);
+            // })
+            // .catch(err => {
+            //         setError(true);
+            //         console.log(`Errore: ${err.message}`)
+            //     }
+            // )
+        })()
+
         // stoppo lo spinner
         setFetching(false)
     }, []);
 
+    const prova = useProva(fetching, error, people);
 
     return (
         <Container>
@@ -138,22 +74,24 @@ const App: React.FC = props => {
                     {/*Prima colonna*/}
 
                     {/*se fetching è true allora non ha finito di caricare i dati e lo spinner è attivo*/}
-                    {fetching &&
-                    <div className={classes.center}>
-                        <CircularProgress color="primary"/>
-                    </div>
-                    }
+                    <div>
+                        {prova.fetching &&
+                        <div className={classes.center}>
+                            <CircularProgress color="primary"/>
+                        </div>
+                        }
 
-                    {error &&
-                    <div className={classes.center}>
-                        <h2>Errore durante il caricamento </h2>
-                    </div>
-                    }
+                        {prova.error &&
+                        <div className={classes.center}>
+                            <h2>Errore durante il caricamento </h2>
+                        </div>
+                        }
 
-                    {!fetching && !error && < HomeContainer people={people}/>}
-                    <div className="col">
-                        {/*seconda colonna*/}
-                        col2
+                        {!prova.fetching && !prova.error && < HomeContainer people={people}/>}
+                        <div className="col">
+                            {/*seconda colonna*/}
+                            col2
+                        </div>
                     </div>
                 </div>
             </div>
