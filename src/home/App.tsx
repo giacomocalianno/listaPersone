b5542cb73db78a1b22eefe724eeedabccce52698
Vito Manuguerra <vito.manuguerra@soft.it>
2021-09-07 16:32:54 +0200

    wip work in progress

--- src/home/App.tsx
+++ src/home/App.tsx
@@ -14,7 +14,7 @@ export interface IPerson {
     birthCity: string
     birthCoutry: string
     id: string,
-    checked?: string
+    checked?: boolean
 }
 
 const useStyles = makeStyles({
@@ -27,13 +27,38 @@ const useStyles = makeStyles({
     }
 })
 
+/*
+    [persone, setPersone] => [persone checked/uncheck]
+
+    personeChecked = persone.filter(persona => persona.checked)
+    personeUncheck = persone.filter(persona => !persona.checked)
+
+    //modifica
+    3 -> perone.map((persona, i) => persona.id === 3 ? ({...persona,checked: !persona.checked}) : persona)
+
+    handleClickPersona(idPersona: number) {
+        const personeAggiornate = perone.map((persona, i) => persona.id === 3 ? ({...persona,checked: !persona.checked}) : persona)
+        setPersone(personeAggiornate)
+    }
+
+    // parte 2 usare useMemo
+*/
+
+
+
 const App: React.FC = props => {
+
+    // persone checked
     let arrayPeople: IPerson[] = [];
 
+    // tutte le persone
     const [people, setPeople] = useState<IPerson[]>([]);
+    //
     const [clickedPeople, setClickedPeople] = useState<IPerson[]>([]);
+
     const [fetching, setFetching] = useState<Boolean>(true);
     const [error, setError] = useState<Boolean>(false);
+
     const [checked, setChecked] = useState(true)
     const [visible, setVisible] = useState(false)
 
@@ -47,8 +72,9 @@ const App: React.FC = props => {
             if (!res.ok) {
                 setError(true)
             }
-            const data = await res.json();
-            setPeople(data)
+            const data: IPerson[] = await res.json();
+            // normalizzo le persone
+            setPeople(data.map(persona => ({...persona, checked: true})))
         })()
 
         // stoppo lo spinner
